01d6195ce26d981266ce8d060b424520ffbd4ce7
Angel Ambi <angelambisilva@gmail.com>
2023-08-29 21:19:00 -0400

    Change API link to include forecast and AQI

--- src/modules/dataRecieverModule.js
+++ src/modules/dataRecieverModule.js
@@ -5,7 +5,9 @@ function weatherModule(api){
     async function getWeatherData(){
         let [location, units] = grabInputs()
     
-        let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`
+        // let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`
+        let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=yes&alerts=no
+        `
     
         let res = await fetch(url)
         let data = await res.json()
