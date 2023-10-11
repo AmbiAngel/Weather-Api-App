25775e7a772813c3bcaf3353ccf3d828285c30f5
Angel Ambi <angelambisilva@gmail.com>
2023-10-10 22:46:54 -0400

    Add functionality to the unit toggle from F to C, and mph to kph

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import {weatherModule} from './modules/dataRecieverModule'
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
+let lastWeatherData = {}
 
 let inputForm = document.querySelector('.input-form')
+let isCelsius = document.querySelector('#units-input')
 
 //Weather info Elements
 let conditionElement = document.querySelector('.condition')
@@ -29,14 +31,30 @@ let weatherApiSession = weatherModule(apiKey)
 
 inputForm.addEventListener('submit', handleInputForm)
 
+isCelsius.addEventListener('click',handleTempSwitch)
+
+function handleTempSwitch(e){
+    handleRenderDataByUnits(lastWeatherData)
+}
+
+function handleRenderDataByUnits(data){
+    if(isCelsius.checked){
+        RenderDOM.renderCurrentWeatherInfo_C(data)
+        RenderDOM.renderForecastInfo_C(data)
+    }
+    else{
+        RenderDOM.renderCurrentWeatherInfo_F(data)
+        RenderDOM.renderForecastInfo_F(data)
+    }
+}
+
 async function handleInputForm(e){
     e.preventDefault()
     let [location, units] = grabInputs()
     let data = await weatherApiSession.getWeatherData(location, units)
+    lastWeatherData =  await data
     console.log(data);
-    RenderDOM.renderCurrentWeatherInfo(data)
-    RenderDOM.renderForecastInfo(data)
-    
+    handleRenderDataByUnits(data)
 }
 
 function grabInputs(){
@@ -54,7 +72,7 @@ function removeAllChildNodes(parent) {
 
 
 class RenderDOM{
-    static renderCurrentWeatherInfo(data){
+    static renderCurrentWeatherInfo_F(data){
         iconElement.setAttribute('src', 'https:' + data.current.condition.icon)
         conditionElement.textContent = data.current.condition.text
         cityElement.textContent = data.location.name
@@ -74,7 +92,27 @@ class RenderDOM{
         windElement.textContent = `${data.current.wind_mph}mph`
     }
 
-    static renderForecastInfo(data){
+    static renderCurrentWeatherInfo_C(data){
+        iconElement.setAttribute('src', 'https:' + data.current.condition.icon)
+        conditionElement.textContent = data.current.condition.text
+        cityElement.textContent = data.location.name
+        stateElement.textContent = data.location.region
+        countryElement.textContent = data.location.country
+
+        let dateObj = new Date(data.location.localtime)
+        let dateTime = data.location.localtime.slice(11)
+        timeElement.textContent = `${days[dateObj.getDay()]} ${dateTime}`
+
+        // console.log(dateObj.toJSON());
+        // timeElement.textContent = data.location.localtime
+
+        temperatureElement.textContent = `${data.current.temp_c}C`
+        humidityElement.textContent = `${data.current.humidity}%`
+        rainElement.textContent = `${data.forecast.forecastday[0].day.daily_chance_of_rain}%`
+        windElement.textContent = `${data.current.wind_kph}kph`
+    }
+
+    static renderForecastInfo_F(data){
         removeAllChildNodes(forecastContainer)
 
         let forecastArr = data.forecast.forecastday
@@ -107,6 +145,40 @@ class RenderDOM{
             forecastContainer.appendChild(forecastDayContainer)
         })
     }
+
+    static renderForecastInfo_C(data){
+        removeAllChildNodes(forecastContainer)
+
+        let forecastArr = data.forecast.forecastday
+        forecastArr.forEach(day => {
+            // console.log(day.date);
+            // console.log(day.day.condition.text);
+            // console.log(day.day.avgtemp_f);
+
+            let forecastDayContainer = document.createElement('div')
+            forecastDayContainer.classList.add('forecast-day-container')
+
+            let forecastIcon = document.createElement('img')
+            forecastIcon.setAttribute('src','https:' + day.day.condition.icon)
+            forecastDayContainer.appendChild(forecastIcon)
+
+            let forecastDate = document.createElement('p')
+            let dateObj = new Date(day.date.replace(/-/g, '\/')) //Date constructor needs hyphens replaced by dashes otherwise the constructor will be one day off
+            forecastDate.textContent = days[dateObj.getDay()]
+            forecastDayContainer.appendChild(forecastDate)
+
+            let forecastCondition = document.createElement('p')
+            forecastCondition.textContent = day.day.condition.text
+            forecastDayContainer.appendChild(forecastCondition)
+
+            let forecastTemp = document.createElement('p')
+            forecastTemp.textContent = `${day.day.avgtemp_c}C`
+            forecastDayContainer.appendChild(forecastTemp)
+            
+
+            forecastContainer.appendChild(forecastDayContainer)
+        })
+    }
 }
 
 
@@ -114,8 +186,9 @@ class RenderDOM{
 async function initialLoad(){
     let initialData = await weatherApiSession.getWeatherData('New York', 'F')
     console.log(initialData);
-    RenderDOM.renderCurrentWeatherInfo(initialData)
-    RenderDOM.renderForecastInfo(initialData)
+    lastWeatherData = initialData
+    RenderDOM.renderCurrentWeatherInfo_F(initialData)
+    RenderDOM.renderForecastInfo_F(initialData)
 }
 
 initialLoad()
